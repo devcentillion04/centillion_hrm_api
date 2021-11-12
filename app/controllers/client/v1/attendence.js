62d6d294df114e63643e88d9f02f5c9c4ce9049c
Mehul Gaudana <devcentillion04@gmail.com>
2021-11-12 17:34:24 +0530

    attendance update

--- app/controllers/client/v1/attendence.js
+++ app/controllers/client/v1/attendence.js
@@ -1,5 +1,4 @@
 const moment = require("moment-timezone");
-const { token } = require("morgan");
 const Attendance = require("../../../models/attendence");
 
 class AttendanceController {
@@ -14,58 +13,54 @@ class AttendanceController {
     try {
       let criteria = {
         workDate: moment().startOf("day").utc(true).add("days"),
-        userId: token.userId,
+        ...req.params._id,
       };
-      
       let attendance = await Attendance.findOne(criteria);
       if (attendance) {
-        let date = moment()
-          .utc(true)
-          let payload={
-            userId: req.currentUser._id,
-            clockOut:date,
-      }
-      let dataTime;
+        let date = moment().utc(true)
+        let payload = {
+          userId: req.currentUser._id,
+          clockOut: date,
+        }
+        let dataTime;
+        let time;
         let b = attendance.entry
-        let abvc=b[b.length-1]
-        if(abvc?.Out != undefined){
+        let abvc = b[b.length - 1]
+        if (abvc?.Out != undefined) {
           dataTime = {
-            $push:{
-              entry:{ 
+            $push: {
+              entry: {
                 In: moment().utc(true)
               }
             }
           }
+
         }
-        else if(abvc?.In != undefined){
+        else if (abvc?.In != undefined) {
           dataTime = {
-            $push:{
-              entry:{ 
+            $push: {
+              entry: {
                 Out: moment().utc(true)
               }
             }
           }
+          time = moment().diff(moment(abvc?.In), "minutes")
+          console.log("time", time)
         }
-        else{
+        else {
           dataTime = {
-            $push:{
-              entry:{ 
+            $push: {
+              entry: {
                 In: moment().utc(true)
               }
             }
           }
         }
-        let diff = moment(abvc?.Out ).diff(abvc?.In, "seconds"); 
-        if(abvc?.Out){
-          let b = diff + moment(abvc?.Out ).diff(abvc?.In, "seconds"); 
-          console.log(b)
-        }else{
-          console.log("sajaslkakldakld")
-        }          
+        let diff = moment(abvc?.Out).diff(abvc?.In, "minutes");
         let updateAttendance = await Attendance.findOneAndUpdate(
           {
             criteria,
-            payload,
+            ...payload,
             workingHours: diff,
             upsert: true,
             new: true,
@@ -95,7 +90,7 @@ class AttendanceController {
   async show(req, res) {
     try {
       const { id } = req.params;
-      let data = await Attendance.findById(id).populate({ path:"userId"}).populate({path:"userId",select:"email"});
+      let data = await Attendance.findById(id).populate({ path: "userId" }).populate({ path: "userId", select: "email" });
       return res.status(200).json({ success: true, data: data });
     } catch (error) {
       return res.status(500).json({ success: false, message: error.message });
