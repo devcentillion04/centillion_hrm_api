35b78584bdbc1fff37f4bc17f31711acc299e120
Jignesh Foon <jigneshfoon@192.168.1.9>
2022-01-03 10:23:05 +0530

    change the database connection

--- app/config/db/connection.js
+++ app/config/db/connection.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
-
+let CONNECTION_URL = `${process.env.MONGO_URL}/${process.env.MONGO_DB}?authSource=admin`;
 module.exports = mongoose
-  .connect(`${process.env.MONGO_URL}/${process.env.MONGO_DB}`)
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("DATABASE CONNECTED");
     console.log("=================================");
   })
   .catch((error) => {
+    console.log(error);
     console.log("DATABASE NOT CONNCTED");
     console.log("=================================");
   });
